bfcf0e4c8e9410b32dfed9544f310d98884729bc
Cesar Lopez Cortes <cesarlopezcortes@hotmail.com>
2020-07-23 16:19:54 -0500

    Masked textbox color behavior added

--- Code/VbNet/CiancaMantto/CiancaMantto/Pantallas/Reportes Maquinaria/ScrEditMaq.vb
+++ Code/VbNet/CiancaMantto/CiancaMantto/Pantallas/Reportes Maquinaria/ScrEditMaq.vb
@@ -142,33 +142,6 @@ Public Class ScrEditMaq
 
     End Sub
 
-    ''' <summary>
-    ''' Re inicia los datos del cbo
-    ''' Consulta a la tabla de inventarios_af
-    ''' Carga la tabla y actualiza los valores
-    ''' Pone el index en 0
-    ''' </summary>
-    Private Sub FillSerialAF()
-
-        'Reset
-        buscar.cboMaqDS.Reset()
-
-        'Llamamos a la consulta de los datos
-        buscar.getSerialMaq()
-
-        'Cargamos los datos de la tabla en el combo para actualizar
-        cboSerial.DataSource = buscar.cboMaqDS.Tables("MAQ")
-
-        'Cargamos los datos de la columna apellidos en el combo para actualizar
-        cboSerial.DisplayMember = "Serie"
-
-        'Index a cero
-        'cboPersonal.SelectedIndex = 0
-
-        cboSerial.Text = "No. de Serie"
-
-    End Sub
-
 #End Region
 
 #Region "EVENTOS"
@@ -186,6 +159,78 @@ Public Class ScrEditMaq
 
         'Referente a los cbo
         FillCboRepMaq()
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el fondo a un color obscuro
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskCantidad_GotFocus(sender As Object, e As EventArgs) Handles mskCantidad.GotFocus
+
+        'Color Obscuro
+        mskCantidad.BackColor = Color.FromArgb(23, 42, 58)
+
+    End Sub
+
+    ''' <summary>
+    ''' Regresa el color de fondo al color original
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskCantidad_LostFocus(sender As Object, e As EventArgs) Handles mskCantidad.LostFocus
+
+        'Color Original
+        mskCantidad.BackColor = Color.FromArgb(9, 188, 138)
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el fondo a un color obscuro
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskHoraEntrega_GotFocus(sender As Object, e As EventArgs) Handles mskHoraEntrega.GotFocus
+
+        'Color Obscuro
+        mskHoraEntrega.BackColor = Color.FromArgb(23, 42, 58)
+
+    End Sub
+
+    ''' <summary>
+    ''' Regresa el color de fondo al color original
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskHoraEntrega_LostFocus(sender As Object, e As EventArgs) Handles mskHoraEntrega.LostFocus
+
+        'Color Original
+        mskHoraEntrega.BackColor = Color.FromArgb(9, 188, 138)
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el fondo a un color obscuro
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskHoraFalla_GotFocus(sender As Object, e As EventArgs) Handles mskHoraFalla.GotFocus
+
+        'Color Obscuro
+        mskHoraFalla.BackColor = Color.FromArgb(23, 42, 58)
+
+    End Sub
+
+    ''' <summary>
+    ''' Regresa el color de fondo al color original
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub mskHoraFalla_LostFocus(sender As Object, e As EventArgs) Handles mskHoraFalla.LostFocus
+
+        'Color Original
+        mskHoraFalla.BackColor = Color.FromArgb(9, 188, 138)
 
     End Sub
 
