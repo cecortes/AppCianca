38ee069663b2669bc0205d1a51438e71f5688a5a
Cesar Lopez Cortes <cesarlopezcortes@hotmail.com>
2020-08-20 21:22:59 -0500

    Edit and Del screens added

--- Code/VbNet/CiancaReq/Pantallas/Req/scrEdit.Designer.vb
+++ Code/VbNet/CiancaReq/Pantallas/Req/scrEdit.Designer.vb
@@ -22,17 +22,39 @@ Partial Class scrEdit
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.pnlTop = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.BunifuSeparator1 = New Bunifu.Framework.UI.BunifuSeparator()
         Me.pnlBot = New System.Windows.Forms.Panel()
+        Me.pnlWr = New System.Windows.Forms.Panel()
+        Me.pnlSearch = New System.Windows.Forms.Panel()
+        Me.lblNom = New System.Windows.Forms.Label()
+        Me.cboNom = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dtpIni = New Bunifu.Framework.UI.BunifuDatepicker()
+        Me.dtpFin = New Bunifu.Framework.UI.BunifuDatepicker()
+        Me.btnSearch = New Bunifu.Framework.UI.BunifuImageButton()
         Me.btnCancel = New Bunifu.Framework.UI.BunifuImageButton()
         Me.btnAceptar = New Bunifu.Framework.UI.BunifuImageButton()
-        Me.pnlWr = New System.Windows.Forms.Panel()
+        Me.BunifuSeparator2 = New Bunifu.Framework.UI.BunifuSeparator()
+        Me.pnlDatos = New System.Windows.Forms.Panel()
+        Me.cboProv = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.pnlData = New System.Windows.Forms.Panel()
+        Me.dgvItems = New Bunifu.Framework.UI.BunifuCustomDataGrid()
         Me.pnlTop.SuspendLayout()
         Me.pnlBot.SuspendLayout()
+        Me.pnlWr.SuspendLayout()
+        Me.pnlSearch.SuspendLayout()
+        CType(Me.btnSearch, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.btnCancel, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.btnAceptar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlDatos.SuspendLayout()
+        Me.pnlData.SuspendLayout()
+        CType(Me.dgvItems, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'pnlTop
@@ -47,6 +69,7 @@ Partial Class scrEdit
         '
         'Label1
         '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Poppins", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
@@ -58,6 +81,8 @@ Partial Class scrEdit
         '
         'BunifuSeparator1
         '
+        Me.BunifuSeparator1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BunifuSeparator1.BackColor = System.Drawing.Color.Transparent
         Me.BunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
         Me.BunifuSeparator1.LineThickness = 1
@@ -79,6 +104,127 @@ Partial Class scrEdit
         Me.pnlBot.Size = New System.Drawing.Size(870, 58)
         Me.pnlBot.TabIndex = 3
         '
+        'pnlWr
+        '
+        Me.pnlWr.Controls.Add(Me.pnlData)
+        Me.pnlWr.Controls.Add(Me.pnlDatos)
+        Me.pnlWr.Controls.Add(Me.pnlSearch)
+        Me.pnlWr.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlWr.Location = New System.Drawing.Point(0, 39)
+        Me.pnlWr.Name = "pnlWr"
+        Me.pnlWr.Size = New System.Drawing.Size(870, 613)
+        Me.pnlWr.TabIndex = 4
+        '
+        'pnlSearch
+        '
+        Me.pnlSearch.Controls.Add(Me.BunifuSeparator2)
+        Me.pnlSearch.Controls.Add(Me.btnSearch)
+        Me.pnlSearch.Controls.Add(Me.dtpFin)
+        Me.pnlSearch.Controls.Add(Me.dtpIni)
+        Me.pnlSearch.Controls.Add(Me.Label2)
+        Me.pnlSearch.Controls.Add(Me.Label4)
+        Me.pnlSearch.Controls.Add(Me.cboNom)
+        Me.pnlSearch.Controls.Add(Me.lblNom)
+        Me.pnlSearch.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlSearch.Location = New System.Drawing.Point(0, 0)
+        Me.pnlSearch.Name = "pnlSearch"
+        Me.pnlSearch.Size = New System.Drawing.Size(870, 55)
+        Me.pnlSearch.TabIndex = 0
+        '
+        'lblNom
+        '
+        Me.lblNom.AutoSize = True
+        Me.lblNom.Font = New System.Drawing.Font("Poppins", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNom.ForeColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.lblNom.Location = New System.Drawing.Point(3, 3)
+        Me.lblNom.Name = "lblNom"
+        Me.lblNom.Size = New System.Drawing.Size(55, 28)
+        Me.lblNom.TabIndex = 1
+        Me.lblNom.Text = "Folio:"
+        '
+        'cboNom
+        '
+        Me.cboNom.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cboNom.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cboNom.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.cboNom.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cboNom.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboNom.ForeColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.cboNom.FormattingEnabled = True
+        Me.cboNom.Location = New System.Drawing.Point(64, 2)
+        Me.cboNom.Name = "cboNom"
+        Me.cboNom.Size = New System.Drawing.Size(183, 33)
+        Me.cboNom.TabIndex = 2
+        Me.cboNom.Text = "No. de Folio"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Poppins", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(250, 3)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 28)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Fecha Inicio:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(515, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 28)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Fecha Final:"
+        '
+        'dtpIni
+        '
+        Me.dtpIni.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.dtpIni.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.dtpIni.BorderRadius = 0
+        Me.dtpIni.ForeColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.dtpIni.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpIni.FormatCustom = Nothing
+        Me.dtpIni.Location = New System.Drawing.Point(373, 3)
+        Me.dtpIni.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.dtpIni.Name = "dtpIni"
+        Me.dtpIni.Size = New System.Drawing.Size(140, 28)
+        Me.dtpIni.TabIndex = 7
+        Me.dtpIni.Value = New Date(2020, 8, 20, 17, 55, 54, 133)
+        '
+        'dtpFin
+        '
+        Me.dtpFin.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.dtpFin.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.dtpFin.BorderRadius = 0
+        Me.dtpFin.ForeColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.dtpFin.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFin.FormatCustom = Nothing
+        Me.dtpFin.Location = New System.Drawing.Point(632, 3)
+        Me.dtpFin.Margin = New System.Windows.Forms.Padding(4, 9, 4, 9)
+        Me.dtpFin.Name = "dtpFin"
+        Me.dtpFin.Size = New System.Drawing.Size(140, 28)
+        Me.dtpFin.TabIndex = 8
+        Me.dtpFin.Value = New Date(2020, 8, 20, 17, 55, 54, 133)
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSearch.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.btnSearch.Image = Global.CiancaReq.My.Resources.Resources.loupe
+        Me.btnSearch.ImageActive = Nothing
+        Me.btnSearch.Location = New System.Drawing.Point(802, -1)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(40, 36)
+        Me.btnSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.btnSearch.TabIndex = 9
+        Me.btnSearch.TabStop = False
+        Me.btnSearch.Zoom = 20
+        '
         'btnCancel
         '
         Me.btnCancel.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
@@ -94,6 +240,7 @@ Partial Class scrEdit
         '
         'btnAceptar
         '
+        Me.btnAceptar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnAceptar.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
         Me.btnAceptar.Image = Global.CiancaReq.My.Resources.Resources.add
         Me.btnAceptar.ImageActive = Nothing
@@ -105,13 +252,100 @@ Partial Class scrEdit
         Me.btnAceptar.TabStop = False
         Me.btnAceptar.Zoom = 20
         '
-        'pnlWr
+        'BunifuSeparator2
         '
-        Me.pnlWr.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.pnlWr.Location = New System.Drawing.Point(0, 39)
-        Me.pnlWr.Name = "pnlWr"
-        Me.pnlWr.Size = New System.Drawing.Size(870, 613)
-        Me.pnlWr.TabIndex = 4
+        Me.BunifuSeparator2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BunifuSeparator2.BackColor = System.Drawing.Color.Transparent
+        Me.BunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.BunifuSeparator2.LineThickness = 1
+        Me.BunifuSeparator2.Location = New System.Drawing.Point(9, 36)
+        Me.BunifuSeparator2.Margin = New System.Windows.Forms.Padding(4, 7, 4, 7)
+        Me.BunifuSeparator2.Name = "BunifuSeparator2"
+        Me.BunifuSeparator2.Size = New System.Drawing.Size(852, 20)
+        Me.BunifuSeparator2.TabIndex = 10
+        Me.BunifuSeparator2.Transparency = 255
+        Me.BunifuSeparator2.Vertical = False
+        '
+        'pnlDatos
+        '
+        Me.pnlDatos.Controls.Add(Me.cboProv)
+        Me.pnlDatos.Controls.Add(Me.Label7)
+        Me.pnlDatos.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlDatos.Location = New System.Drawing.Point(0, 55)
+        Me.pnlDatos.Name = "pnlDatos"
+        Me.pnlDatos.Size = New System.Drawing.Size(870, 41)
+        Me.pnlDatos.TabIndex = 2
+        '
+        'cboProv
+        '
+        Me.cboProv.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cboProv.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cboProv.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cboProv.BackColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.cboProv.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cboProv.Font = New System.Drawing.Font("Poppins", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboProv.ForeColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.cboProv.FormattingEnabled = True
+        Me.cboProv.Location = New System.Drawing.Point(567, 3)
+        Me.cboProv.Name = "cboProv"
+        Me.cboProv.Size = New System.Drawing.Size(295, 33)
+        Me.cboProv.TabIndex = 8
+        Me.cboProv.Text = "Razón Social"
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Poppins", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.Label7.Location = New System.Drawing.Point(460, 4)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(101, 28)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Proveedor:"
+        '
+        'pnlData
+        '
+        Me.pnlData.Controls.Add(Me.dgvItems)
+        Me.pnlData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlData.Location = New System.Drawing.Point(0, 96)
+        Me.pnlData.Name = "pnlData"
+        Me.pnlData.Size = New System.Drawing.Size(870, 517)
+        Me.pnlData.TabIndex = 3
+        '
+        'dgvItems
+        '
+        Me.dgvItems.AllowUserToAddRows = False
+        Me.dgvItems.AllowUserToDeleteRows = False
+        Me.dgvItems.AllowUserToOrderColumns = True
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.dgvItems.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvItems.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.dgvItems.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.dgvItems.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Poppins", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvItems.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.dgvItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvItems.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvItems.DoubleBuffered = True
+        Me.dgvItems.EnableHeadersVisualStyles = False
+        Me.dgvItems.HeaderBgColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.dgvItems.HeaderForeColor = System.Drawing.Color.FromArgb(CType(CType(171, Byte), Integer), CType(CType(214, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.dgvItems.Location = New System.Drawing.Point(0, 0)
+        Me.dgvItems.Name = "dgvItems"
+        Me.dgvItems.ReadOnly = True
+        Me.dgvItems.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        Me.dgvItems.RowHeadersWidth = 51
+        Me.dgvItems.RowTemplate.Height = 24
+        Me.dgvItems.Size = New System.Drawing.Size(870, 517)
+        Me.dgvItems.TabIndex = 4
         '
         'scrEdit
         '
@@ -131,8 +365,16 @@ Partial Class scrEdit
         Me.pnlTop.ResumeLayout(False)
         Me.pnlTop.PerformLayout()
         Me.pnlBot.ResumeLayout(False)
+        Me.pnlWr.ResumeLayout(False)
+        Me.pnlSearch.ResumeLayout(False)
+        Me.pnlSearch.PerformLayout()
+        CType(Me.btnSearch, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.btnCancel, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.btnAceptar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlDatos.ResumeLayout(False)
+        Me.pnlDatos.PerformLayout()
+        Me.pnlData.ResumeLayout(False)
+        CType(Me.dgvItems, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -144,4 +386,18 @@ Partial Class scrEdit
     Friend WithEvents btnCancel As Bunifu.Framework.UI.BunifuImageButton
     Friend WithEvents btnAceptar As Bunifu.Framework.UI.BunifuImageButton
     Friend WithEvents pnlWr As Panel
+    Friend WithEvents pnlSearch As Panel
+    Friend WithEvents lblNom As Label
+    Friend WithEvents cboNom As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnSearch As Bunifu.Framework.UI.BunifuImageButton
+    Friend WithEvents dtpFin As Bunifu.Framework.UI.BunifuDatepicker
+    Friend WithEvents dtpIni As Bunifu.Framework.UI.BunifuDatepicker
+    Friend WithEvents BunifuSeparator2 As Bunifu.Framework.UI.BunifuSeparator
+    Friend WithEvents pnlDatos As Panel
+    Friend WithEvents cboProv As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents pnlData As Panel
+    Friend WithEvents dgvItems As Bunifu.Framework.UI.BunifuCustomDataGrid
 End Class
